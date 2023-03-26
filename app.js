cf3094c1ef40a909c5a86f0cb29e723101a36f37
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-03-26 13:58:13 +0200

    setup express to start working server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bookRoutes = require('./routes/bookRoutes');
+
+// express app
+const app = express();
+app.listen(3001);
+
+app.get('/', (req, res) => {
+	res.send('hello')
+})
